23fff1363b0d809c3859489175ba95523aaedae6
Melhed <82815800+Melhed@users.noreply.github.com>
2023-01-30 16:53:15 +0100

    Created loginService & components folder

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable, of, switchMap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Trainer } from '../models/trainer.model';
+
+const { apiTrainers, apiKey } = environment;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  constructor(private readonly http: HttpClient) {}
+
+  // Checks if user exists, if not: create user.
+  login(username: string): Observable<Trainer> {
+    return this.checkUser(username).pipe(
+      switchMap((trainer: Trainer | undefined) => {
+        if (trainer === undefined) {
+          return this.createUser(username);
+        }
+        return of(trainer);
+      })
+    );
+  }
+
+  // Checks if user exists
+  private checkUser(username: string): Observable<Trainer | undefined> {
+    return this.http
+      .get<Trainer[]>(`${apiTrainers}?username=${username}`)
+      .pipe(map((res: Trainer[]) => res.pop()));
+  }
+
+  // Creates new user
+  private createUser(username: string): Observable<Trainer> {
+    const user = {
+      username,
+      pokemon: [],
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-api-key': apiKey,
+    });
+
+    return this.http.post<Trainer>(apiTrainers, user, { headers });
+  }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiTrainers: 'https://bling-bling.herokuapp.com/trainers',
+};
