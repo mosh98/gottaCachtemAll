d9d7d3fdc890194a764ba23a225719bc9beb014c
mosh98 <mosleh.edu@gmail.com>
2023-01-30 15:02:37 +0100

    trainer page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TrainerPagePage } from './pages/trainer-page/trainer-page.page';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainerPagePage
   ],
+
   imports: [
     BrowserModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
---
+++ src/app/pages/trainer-page/trainer-page.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer-page',
+  templateUrl: './trainer-page.page.html',
+  styleUrls: ['./trainer-page.page.css']
+})
+export class TrainerPagePage {
+
+}
