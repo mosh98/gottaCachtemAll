3980a3e7154c4ee4018de5e3bc68c9e6c61bb565
Melhed <82815800+Melhed@users.noreply.github.com>
2023-01-31 14:35:21 +0100

    Fix: Stores trainer in local storage, not session

--- src/app/utils/storage.utils.ts
+++ src/app/utils/storage.utils.ts
@@ -1,17 +1,17 @@
 export class StorageUtil {
-  // Stores json of trainer to session storage.
+  // Stores json of trainer to local storage.
   static storageSave<T>(key: string, value: T): void {
-    sessionStorage.setItem(key, JSON.stringify(value));
+    localStorage.setItem(key, JSON.stringify(value));
   }
 
-  // Fetches and parses json of trainer from session storage.
+  // Fetches and parses json of trainer from local storage.
   static storageRead<T>(key: string): T | undefined {
-    const storedValue = sessionStorage.getItem(key);
+    const storedValue = localStorage.getItem(key);
     try {
       if (storedValue) return JSON.parse(storedValue);
       return undefined;
     } catch (e) {
-      sessionStorage.removeItem(key);
+      localStorage.removeItem(key);
       return undefined;
     }
   }
